5585a943b3bc5f41c196e51d1eac71fc9825aaaf
Zilvinas <zil@Gretas-MacBook-Pro.local>
2022-05-10 12:53:17 +0300

    2.1: Course information step6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,52 @@
+
+
+const Header = ({ course }) => <h1>{course}</h1>
+  
+const Content =({ course }) =>{
+  console.log("from conctent", course)
+  return(
+    
+    <p>{course.name} {course.exercises} </p>
+  )
+}
+
+const Course = ({ course }) =>{
+  console.log("From Coures: ",course)
+  const parts = course.parts
+  console.log("parts from course", parts)
+  return(
+   <div>
+     <Header course={course.name} />
+        {parts.map(parts => 
+          <Content key={parts.id} course={parts} />
+        )}
+   </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+export default App
