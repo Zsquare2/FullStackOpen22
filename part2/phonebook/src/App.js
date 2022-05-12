a233e5192d8439c6c89cf5e9b8df364f98528767
Zilvinas <zil@Gretas-MacBook-Pro.local>
2022-05-12 14:17:40 +0300

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,52 +1,72 @@
 import { useState } from 'react'
 
-const Names = ({ name }) => <tr><td>{name.name} {name.number}</td></tr>
+const Names = ({ name }) => <tr><td>{name.name}</td><td>{name.number}</td></tr>
+
 
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' ,
-      // id: 1
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ])
+ 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchValue, setSearchValue] = useState('')
+
+  const namesArray = persons.map(names => names.name)
+
 
   const handleNameChange = (event) =>{
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
   
   const handleNumberChange = (event) =>{
-    console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
+  const handleSearchValueChange = (event) =>{
+    setSearchValue(event.target.value)
+  }
+
+  const nameSearch = 
+  (persons.filter((search) => (search.name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1)))
+
   const addName = (event) =>{
     event.preventDefault()
-    const namesArray = persons.map(names => names.name)
 
     console.log("button clicked", event.target)
     console.log("can i add name :", newName )
     // nameExists()
       if (namesArray.includes(newName)){
-        console.log("name exists")
+        console.log("name already exists")
         window.alert(`${newName} is already added to phonebook`);
 
       }else{
-      const newObject = {
-        name: newName,
-        number: newNumber,
-        // id: persons.length +1
-      }
-      setPersons(persons.concat(newObject))
-
-      setNewName("")
-      setNewNumber("")
+        const newObject = {
+          name: newName,
+          number: newNumber,
+          id: persons.length +1
+        }
+        setPersons(persons.concat(newObject))
+        setNewName("")
+        setNewNumber("")
     }}
 
   return (
     <div>
+      <h2>Search</h2>
+      <div>
+      filter shown with <input  
+        value={searchValue}
+        onChange={handleSearchValueChange}
+        />
+      </div>
+      <div>debug search value: {searchValue}</div>
+
+
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
@@ -72,7 +92,7 @@ const App = () => {
       <div>
         <table>
           <tbody>
-            {persons.map((name, index) => 
+            {nameSearch.map((name, index) => 
               <Names key={index} name={name} />
             )}
           </tbody>
