46503a3803f3266e7fd4fbc0fd1b5e9952d16340
Zilvinas <zil@Gretas-MacBook-Pro.local>
2022-06-08 17:52:37 +0300

    uibuild

---
+++ part3/phonebook-backend/build/precache-manifest.dedc4f9a88e0d001088d0d8efc758055.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f47bbfb2b81f21fd76c8",
+    "url": "/static/css/main.b51d6369.chunk.css"
+  },
+  {
+    "revision": "f47bbfb2b81f21fd76c8",
+    "url": "/static/js/main.f47bbfb2.chunk.js"
+  },
+  {
+    "revision": "42323de52940558adb9d",
+    "url": "/static/js/1.42323de5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9bf7abefb860100851402a8c43b8ef4",
+    "url": "/index.html"
+  }
+];
