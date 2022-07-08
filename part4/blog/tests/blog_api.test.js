161f070481456f242dfa44ef8570be104f6316b6
Zilvinas <zil@Gretas-MacBook-Pro.local>
2022-07-08 12:58:31 +0300

    4.14 Blog list expansions, step1-2

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -18,69 +18,74 @@ beforeEach(async () => {
 })
 
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('Getting notes', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('there are six notes', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(6)
+  })
 })
 
-test('there are six notes', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(6)
-})
-
-test('property of the blog posts is named "id" not ', async () => {
+describe('adding blog', () => {
+  test('property of the blog posts is named "id" not "_id"', async () => {
   const response = await api.get('/api/blogs')
   response.body.forEach(blog => {
     expect(blog.id).toBeDefined()
-  });
-})
-
-test('blog can be added', async () => {
-  const newBlog = {
-    title: "TESTS TITLE",
-    author: "TESTS AUTHOR",
-    url: "TESTS URL",
-    likes: "69"
-  }
-  await api 
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-})
-
-test('if likes property missing, default value to 0', async () => {
-  const newBlog = new Blog({
-    title: "TESTS no likes",
-    author: "TESTS no likes",
-    url: "TESTS likes",
+    });
   })
-  
-  await newBlog.save()
-  const id = newBlog._id.toString()
 
-  blogsAtEnd = await helper.blogsInDb()
-  const checkBlog = blogsAtEnd.find(blog => blog.id === id)
-  expect(checkBlog.likes).toBe(0)
+  test('blog can be added', async () => {
+    const newBlog = {
+      title: "TESTS TITLE",
+      author: "TESTS AUTHOR",
+      url: "TESTS URL",
+      likes: "69"
+    }
+    await api 
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  })
+
+  test('if likes property missing, default value to 0', async () => {
+    const newBlog = new Blog({
+      title: "TESTS no likes",
+      author: "TESTS no likes",
+      url: "TESTS likes",
+    })
+    
+    await newBlog.save()
+    const id = newBlog._id.toString()
+
+    blogsAtEnd = await helper.blogsInDb()
+    const checkBlog = blogsAtEnd.find(blog => blog.id === id)
+    expect(checkBlog.likes).toBe(0)
+
+  })
+
+  test('if no title or url 400 Bad Request', async () => {
+      const missingDataBlog = {
+      author: "TESTS AUTHOR",
+      likes: "69"
+    }
+    await api
+      .post('/api/blogs')
+      .send(missingDataBlog)
+      .expect(400)
+  })
 })
 
-test('if no title or url 400 Bad Request', async () => {
-    const missingDataBlog = {
-    author: "TESTS AUTHOR",
-    likes: "69"
-  }
-  await api
-    .post('/api/blogs')
-    .send(missingDataBlog)
-    .expect(400)
-})
 
 describe('dletion of a blog', () => {
   test('succeeds with code 204 if id is valid', async () => {
